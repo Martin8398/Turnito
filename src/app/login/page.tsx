6a6e603fb8f82abc860ca62fb7d4f6ad8b088f70
Tincho <martinnicolas.larrosa@gmail.com>
2026-03-29 03:34:43 -0300

    creacion de proyecto next con back en suppabase, intento 1 de creacion de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../../lib/supabase";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    router.push("/");
+  };
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <form
+        onSubmit={handleLogin}
+        className="p-6 border rounded-xl shadow w-80"
+      >
+        <h1 className="text-xl font-bold mb-4">Iniciar sesión</h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full mb-3 p-2 border rounded"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Contraseña"
+          className="w-full mb-4 p-2 border rounded"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-black text-white p-2 rounded"
+        >
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+}
